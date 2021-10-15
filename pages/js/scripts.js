00c35b948fe0076913702d1927b59b3353ac3e49
lcmps <lucas1498campos@gmail.com>
2021-10-15 15:59:19 -0300

    fixed uniform card sizing, added css boxes to pokemon types

--- pages/js/scripts.js
+++ pages/js/scripts.js
@@ -34,11 +34,13 @@ function addAllPokemons(data) {
             </div>
             <img src="/sprt/`+ getPokemonSprite(ele.id) + `MS.png" class="card-img imgPokemon">
             <div class="card-body">
-                <p class="card-text pokemonDescription">Type: `+ ele.type +` </p>
+                <div class="pokemonType">
+                `+ getPokemonType(ele.type) +`
+                </div>
                 <div class="divStatus">
-                    <p class="pokemonStatus">HP: `+ ele.base.HP + `</p>
-                    <p class="pokemonStatus">Attack:`+ ele.base.Attack + `</p>
-                    <p class="pokemonStatus">Defense:`+ ele.base.Defense + `</p>
+                    <p class="pokemonStatus"><span class="iconify" data-icon="mdi:cards-heart-outline" style="color: #ff6e5a;"></span>`+ ele.base.HP + `</p>
+                    <p class="pokemonStatus"><span class="iconify" data-icon="mdi:sword" data-rotate="180deg" style="color: #ff6e5a;"></span>`+ ele.base.Attack + `</p>
+                    <p class="pokemonStatus"><span class="iconify" data-icon="mdi:shield-outline" style="color: #ff6e5a;"></span>`+ ele.base.Defense + `</p>
                 </div>
             </div>
         </div>`;
@@ -59,5 +61,86 @@ function getPokemonSprite(idPokemon) {
     }
 
     return idPokemon;
+}
 
+function getPokemonType(types) {
+    var html = "";
+
+    types.forEach(ele => {
+        switch (ele) {
+            case "Fighting":
+                html += `<div class="typeBox fighting">Fighting</div>`
+                break;
+
+            case "Flying":
+                html += `<div class="typeBox flying">Flying</div>`
+                break;
+
+            case "Poison":
+                html += `<div class="typeBox poison">Poison</div>`
+                break;
+
+            case "Ground":
+                html += `<div class="typeBox ground">Ground</div>`
+                break;
+
+            case "Rock":
+                html += `<div class="typeBox rock">Rock</div>`
+                break;
+
+            case "Bug":
+                html += `<div class="typeBox bug">Bug</div>`
+                break;
+
+            case "Ghost":
+                html += `<div class="typeBox ghost">Ghost</div>`
+                break;
+
+            case "Steel":
+                html += `<div class="typeBox steel">Steel</div>`
+                break;
+
+            case "Fire":
+                html += `<div class="typeBox fire">Fire</div>`
+                break;
+
+            case "Water":
+                html += `<div class="typeBox water">Water</div>`
+                break;
+
+            case "Grass":
+                html += `<div class="typeBox grass">Grass</div>`
+                break;
+
+            case "Electric":
+                html += `<div class="typeBox electric">Electric</div>`
+                break;
+
+            case "Psychic":
+                html += `<div class="typeBox psychic">Psychic</div>`
+                break;
+
+            case "Ice":
+                html += `<div class="typeBox ice">Ice</div>`
+                break;
+
+            case "Dragon":
+                html += `<div class="typeBox dragon">Dragon</div>`
+                break;
+
+            case "Dark":
+                html += `<div class="typeBox dark">Dark</div>`
+                break;
+
+            case "Fairy":
+                html += `<div class="typeBox fairy">Fairy</div>`
+                break;
+
+            default:
+                html += `<div class="typeBox normal">Normal</div>`
+                break;
+        }
+    })
+
+    return html
 }
